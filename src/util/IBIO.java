df5cab17daaedd307946fc9bcb0d298827ea67f8
Daniel Vazquez <dvazquez@greengates.edu.mx>
2022-09-24 15:54:26 -0500

    public scope for IBIO methods

--- src/util/IBIO.java
+++ src/util/IBIO.java
@@ -5,21 +5,21 @@ public class IBIO {
     // IBIO Standard Input and Output
     // These methods must be copied into your program(s).
     //===========================================================
-    static void output(String info)
+    public static void output(String info)
     { System.out.println(info); }
-    static void output(char info)
+    public static void output(char info)
     { System.out.println(info); }
-    static void output(byte info)
+    public static void output(byte info)
     { System.out.println(info); }
-    static void output(int info)
+    public static void output(int info)
     { System.out.println(info); }
-    static void output(long info)
+    public static void output(long info)
     { System.out.println(info); }
-    static void output(double info)
+    public static void output(double info)
     { System.out.println(info); }
-    static void output(boolean info)
+    public static void output(boolean info)
     { System.out.println(info); }
-    static String input(String prompt)
+    public static String input(String prompt)
     { String inputLine = "";
         System.out.print(prompt);
         try
@@ -33,47 +33,47 @@ public class IBIO {
         }
         return inputLine;
     }
-    static String inputString(String prompt)
+    public static String inputString(String prompt)
     { return input(prompt); }
-    static String input()
+    public static String input()
     { return input(""); }
-    static int inputInt()
+    public static int inputInt()
     { return inputInt(""); }
-    static double inputDouble()
+    public static double inputDouble()
     { return inputDouble(""); }
-    static char inputChar(String prompt)
+    public static char inputChar(String prompt)
     { char result=(char)0;
         try{result=input(prompt).charAt(0);}
         catch (Exception e){result = (char)0;}
         return result;
     }
-    static byte inputByte(String prompt)
+    public static byte inputByte(String prompt)
     { byte result=0;
         try{result=Byte.valueOf(input(prompt).trim()).byteValue();}
         catch (Exception e){result = 0;}
         return result;
     }
-    static int inputInt(String prompt)
+    public static int inputInt(String prompt)
     { int result=0;
         try{result=Integer.valueOf(
                 input(prompt).trim()).intValue();}
         catch (Exception e){result = 0;}
         return result;
     }
-    static long inputLong(String prompt)
+    public static long inputLong(String prompt)
     { long result=0;
         try{result=Long.valueOf(input(prompt).trim()).longValue();}
         catch (Exception e){result = 0;}
         return result;
     }
-    static double inputDouble(String prompt)
+    public static double inputDouble(String prompt)
     { double result=0;
         try{result=Double.valueOf(
                 input(prompt).trim()).doubleValue();}
         catch (Exception e){result = 0;}
         return result;
     }
-    static boolean inputBoolean(String prompt)
+    public static boolean inputBoolean(String prompt)
     { boolean result=false;
         try{result=Boolean.valueOf(
                 input(prompt).trim()).booleanValue();}
